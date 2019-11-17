416ff2b0462c4e6efaaf1c7da1649a9c3744ee1e
DrYSG <ygutfreund@draper.com>
2019-11-17 18:40:48 -0500

    working aws deploy

--- lambdaKit2/hello-world/app.js
+++ lambdaKit2/hello-world/app.js
@@ -20,7 +20,7 @@ exports.lambdaHandler = async (event, context) => {
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: 'hello world',
+                message: 'Hellow from YSG Lambda!',
                 // location: ret.data.trim()
             })
         }
@@ -31,3 +31,5 @@ exports.lambdaHandler = async (event, context) => {
 
     return response
 };
+
+
